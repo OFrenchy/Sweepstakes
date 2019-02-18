1e0a72a5dc8109d3376ea96f45e8488fe7c8cad0
Dean Bourgeois <deanbourgeois@OFrenchy.lan>
2019-02-17 22:25:42 -0600

    Added ContactContestants to ISweepstakesManager

--- Sweepstakes/ISweepstakesManager.cs
+++ Sweepstakes/ISweepstakesManager.cs
@@ -8,31 +8,91 @@ namespace Sweepstakes
 {
     public class ISweepstakesManager
     {
-        public string sweepstakesName;
-        public string sweepstakesDescription;
-        public string grandPrize;
+        string marketingCompanyName = "Jack, Inc.";
+        string clientCompanyName = "Presley, Inc.";
+        string marketingCompanyTelephoneNumber = "(123) 456-7890";
+        //string ss_ID;
+        //string ss_Name;
+        //string description;
+        //string prize;
+        //DateTime startDate;
+        //DateTime endDateTime;
+        //DateTime drawingDateTimeScheduled;
 
+        string detailsFinePrint = 
+            "Employees and relatives of employees of {clientCompanyName} and {marketingCompanyName} " +
+            "and their subsidiaries are not eligible to enter.\n " +
+            "The sweepstakes begins on {startDate} and ends at {endDateTime}. " +
+            "The drawing will be held on {drawingDateTimeScheduled}. " +
+            "The winning number is not transferrable.";
+        string winningEmailSubject = "Congratulations on your {clientCompanyName} {ss_Name} Sweepstakes Entry";
+        string winningEmailBodyMessage = "Congratulations to {FirstName} {LastName}!\n  " +
+            "You are the winner of the {prize} " +
+            "in the {clientCompanyName} {ss_Name} Sweepstakes!\n " +
+            "Your ticket number {registrationNumber} was picked to win!\n  " +
+            "Please contact {marketingCompanyName} at {marketingCompanyTelephoneNumber} to discuss the details.  \n\n" +
+            "Once again, congratulations!";
+        string nonWinningEmailSubject = "Your {clientCompanyName} {ss_Name} Sweepstakes Entry";
+        string nonWinningEmailBodyMessage = "Unfortunately, {FirstName} {LastName} did not win " +
+            "in the {clientCompanyName} {ss_Name} Sweepstakes.\n  " +
+            "Your ticket number was {registrationNumber}.  The winning ticket, number {winningRegistrationNumber},\n " + 
+            "was randomly picked at {drawingDateTimeActual}\n\n" +
+            "{clientCompanyName} would like to thank you for playing!  We hope you win next time!";
+
+        private string ReplaceFields(string stringToReplaceFields, Contestant contestant, Sweepstakes thisSS)
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            stringBuilder.Append(stringToReplaceFields);
+            stringBuilder.Replace("{clientCompanyName}", clientCompanyName);
+            stringBuilder.Replace("{clientCompanyName}", clientCompanyName);
+            stringBuilder.Replace("{marketingCompanyName}", marketingCompanyName);
+            stringBuilder.Replace("{marketingCompanyTelephoneNumber}", marketingCompanyTelephoneNumber);
+            stringBuilder.Replace("{startDate}", thisSS.StartDate.ToShortDateString());
+            stringBuilder.Replace("{endDateTime}", thisSS.EndDateTime.ToShortDateString());
+            stringBuilder.Replace("{drawingDateTimeScheduled}", thisSS.DrawingDateTimeScheduled.ToShortDateString());
+            stringBuilder.Replace("{ss_Name}", thisSS.Ss_Name);
+            stringBuilder.Replace("{FirstName}", contestant.FirstName);
+            stringBuilder.Replace("{LastName}", contestant.LastName);
+            stringBuilder.Replace("{prize}", thisSS.Prize);
+            stringBuilder.Replace("{registrationNumber}", contestant.RegistrationNumber.ToString());
+            stringBuilder.Replace("{winningRegistrationNumber}", thisSS.WinningRegistrationNumber.ToString());
+            //stringBuilder.Replace("{}", );
+            //stringBuilder.Append("\n");
+            return stringBuilder.ToString();
+        }
 
         public void ContactContestants(Sweepstakes thisSS)
         {
-            foreach (KeyValuePair<int, Contestant> kvp in thisSS.Contestants )
-            {
-
-                Console.WriteLine("Key = {0}, Value = {1}", kvp.Key, kvp.Value);
-            }
-
-            Dictionary<int, Contestant>.ValueCollection valueColl =
-            openWith.Values;
-
             foreach (Contestant contestant in thisSS.Contestants.Values)
-            {    //
+            {   
+                // TODO - change this later to not be nearly duplicate 
+                if (contestant.RegistrationNumber != thisSS.WinningRegistrationNumber)
+                {
+                    string thisMessageSubject = ReplaceFields(thisSS.NonWinningEmailSubject, contestant, thisSS);
+                    string thisMessageBody = ReplaceFields(thisSS.NonWinningEmailSubject, contestant, thisSS);
+                    string messageForConsoleWrite =
+                        "=================================================\nSubject:  \n" +
+                        thisMessageSubject +
+                        "=================================================\nBody:  \n"  +
+                        thisMessageBody;
+                        UserInterface.displayMessage(messageForConsoleWrite, false);
+                }
+                else
+                {
+                    string thisMessageSubject = ReplaceFields(thisSS.WinningEmailSubject, contestant, thisSS);
+                    string thisMessageBody = ReplaceFields(thisSS.WinningEmailBodyMessage, contestant, thisSS);
+                    string messageForConsoleWrite =
+                        "=================================================\nSubject:  \n" +
+                        thisMessageSubject +
+                        "=================================================\nBody:  \n" +
+                        thisMessageBody;
+                    UserInterface.displayMessage(messageForConsoleWrite.ToString(), false);
+                }
 
-                UserInterface.displayMessage(, false);
+
+
 
             }
-
         }
-
-
     }
 }
--- Sweepstakes/Sweepstakes.cs
+++ Sweepstakes/Sweepstakes.cs
@@ -9,20 +9,32 @@ namespace Sweepstakes
     // I may use "SS", "ss_", and "_ss_" as aliases to shorten variable & method names
     public class Sweepstakes     
     {
-        string name;
+        string clientCompanyName;
+        string ss_ID;
+        string ss_Name;
+        string description;
         string prize;
         string detailsFinePrint;
+
         DateTime startDate;
         DateTime endDateTime;
-        DateTime drawingDateTime;
+        DateTime drawingDateTimeScheduled;
+        DateTime drawingDateTimeActual = DateTime.Parse("1/1/1900");
         bool prizePhysicallyAwarded;
-        DateTime prizeTransferredDateTime;
+        DateTime prizeTransferredDateTime = DateTime.Parse("1/1/1900");
         Dictionary<int, Contestant> contestants;
         int winningRegistrationNumber = -1;     // only gets set once
 
-        public Sweepstakes(string sweepstakesName, )
+        //string winningEmailSubject;
+        //string winningEmailBodyMessage;
+        //string nonWinningEmailSubject;
+        //string nonWinningEmailBodyMessage;
+
+
+
+        public Sweepstakes(string sweepstakesName)//, all other data req'd for new SS )
         {
-            this.name = sweepstakesName;
+            this.ss_Name = sweepstakesName;
             contestants = new Dictionary<int, Contestant>();
         }
         public void PrintContestantInfo(Contestant contestant)
@@ -43,30 +55,59 @@ namespace Sweepstakes
             get => contestants;
         }
         
-        public string Name
-        {
-            get => name;
-            set { if (name == null) name = value; }
-        }
+        
 
         public string PickWinner()
         {
-            if (DateTime.Now < endDateTime)
+            if (DateTime.Now < endDateTime || DateTime.Now < drawingDateTimeScheduled)
             {
                 UserInterface.displayMessage($"The end date and time of this sweepstakes is {endDateTime.ToLongDateString()}", true);
                 return null;
             }
             Random random = new Random();
             winningRegistrationNumber = random.Next(1, contestants.Count + 1);
+            drawingDateTimeActual = DateTime.Now; //.ToLongDateString();
             Contestant winner = contestants[winningRegistrationNumber];
-            drawingDateTime = DateTime.Now; //.ToLongDateString();
             return $"{winner.LastName}, {winner.FirstName}, {winner.EmailAddress}, {winner.RegistrationNumber}";
         }
-        public int WinningRegistrationNumber
+        public string Ss_Name
+        {
+            get => ss_Name;
+            set { if (ss_Name == null) ss_Name = value; }
+        }public int WinningRegistrationNumber
         {
             get => winningRegistrationNumber;
         }
-        
+        public string ClientCompanyName { get; }
+        public string Ss_ID { get; }
+        public string Description { get; }
+        public string Prize { get; }
+        //public string detailsFinePrint;
+
+        public DateTime StartDate { get; }
+        public DateTime EndDateTime { get; }
+        public DateTime DrawingDateTimeScheduled { get; }
+        public DateTime DrawingDateTimeActual
+        {
+            get => drawingDateTimeActual;
+            set { if (drawingDateTimeActual.ToShortDateString() == "1/1/1900") drawingDateTimeActual = value; }
+        }
+        public bool PrizePhysicallyAwarded
+        {
+            get => prizePhysicallyAwarded;
+            set { if (!prizePhysicallyAwarded) { prizePhysicallyAwarded = value; } }
+        }
+        public DateTime PrizeTransferredDateTime
+        {
+            get => prizeTransferredDateTime;
+            set { if (prizeTransferredDateTime.ToShortDateString() == "1/1/1900") prizeTransferredDateTime = value; }
+        }
+        public string WinningEmailSubject { get; set; }
+        public string WinningEmailBodyMessage { get; set; }
+        public string NonWinningEmailSubject { get; set; }
+        public string NonWinningEmailBodyMessage { get; set; }
+
+
 
     }
 }
